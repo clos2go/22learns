48784859c9895e46952fb64fd3fbb633105a129f
lopezlbj <carlos.lopez@austinisd.org>
2022-04-04 08:27:03 -0500

    consolidate lessons

---
+++ odinP/01_fizzBuzz/main.js
@@ -0,0 +1,17 @@
+console.clear()
+
+const prompt = require('prompt-sync')()
+
+// const name = prompt('What is your name?')
+// console.log(`Hey ther ${name}`)
+
+// let answer = parseInt(
+//   prompt('Please enter a number you would like to fizzBuzz up to: ')
+// )
+
+// let answer = parseInt(
+//   prompt('Please enter the number you would like to FizzBuzz up to: ')
+// )
+
+const num = prompt('Enter number: ')
+console.log(`your number + 4 = ${num + 4}`)
